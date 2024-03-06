dc504fcf3bfa3e715c7522531b9bcc5ebab2685d
ARROUS Thomas <thomas_arrous@yahoo.fr>
2024-03-06 01:18:42 +0100

    fmt and add Cnf lib

--- lib/graph.ml
+++ lib/graph.ml
@@ -1,3 +1,5 @@
+open SAT_Solver.Cnf
+
 type color = int
 type position = int * int
 type vertex = position * color
@@ -112,9 +114,8 @@ let add_breadth_first_search q lst = q @ lst
 let breadth_first_search g v app =
   search g [] [ v ] add_breadth_first_search app
 
-let get_name_variable (x, y, t, c) dim=
-if true then Printf.sprintf "%i,%i,%i,%i" x y t c else 
-  Printf.sprintf "%i" (x + y * dim  + t * dim * dim + c * dim * dim * dim)
+let get_name_variable (x, y, t, c) dim =
+  Printf.sprintf "%i" (x + (y * dim) + (t * dim * dim) + (c * dim * dim * dim))
 
 let get_value_variable var dim =
   let x = int_of_string var in
@@ -131,36 +132,62 @@ let get_value_variable var dim =
 let get_cnf_only_one_true (vars : string list) : cnf =
   let rec not_all_except var = function
     | [] -> []
-    | hd :: tl ->
-        [(var, false); (hd, false)] :: (not_all_except var tl)
+    | hd :: tl -> [ (var, false); (hd, false) ] :: not_all_except var tl
   in
   let rec generate_exprs = function
     | [] -> []
-    | hd :: tl ->
-        (not_all_except hd tl) @ (generate_exprs tl)
+    | hd :: tl -> not_all_except hd tl @ generate_exprs tl
   in
-  ((List.map (fun x -> (x,true)) vars) :: (generate_exprs vars))
-  
-let check_have_color (x, y, t, c) (possibles_colors:color list) (dim : int) : cnf =
+  List.map (fun x -> (x, true)) vars :: generate_exprs vars
+
+let check_have_color (x, y, t, c) (possibles_colors : color list) (dim : int) :
+    cnf =
   let rec aux res colors =
     match colors with
-    | hd :: tl -> [let var = get_name_variable (x,y,t,c) dim in if hd = c then (var,true) else (var,false)] :: aux res tl 
+    | hd :: tl ->
+        [
+          (let var = get_name_variable (x, y, t, c) dim in
+           if hd = c then (var, true) else (var, false));
+        ]
+        :: aux res tl
     | [] -> []
-  in aux [] possibles_colors
+  in
+  aux [] possibles_colors
 
 let check_have_not_color (x, y, t, c) possibles_colors dim : cnf =
-  [((get_name_variable (x,y,t,c) dim), true)] :: get_cnf_only_one_true ((List.map (fun color -> get_name_variable (x,y,t,color) dim) possibles_colors))
+  [ (get_name_variable (x, y, t, c) dim, true) ]
+  :: get_cnf_only_one_true
+       (List.map
+          (fun color -> get_name_variable (x, y, t, color) dim)
+          possibles_colors)
 
 let or_cnf_dev cnf1 cnf2 =
-  List.fold_left (fun acc clause1 -> acc @ (List.map (fun clause2 -> clause1 @ clause2) cnf2)) [] cnf1
+  List.fold_left
+    (fun acc clause1 -> acc @ List.map (fun clause2 -> clause1 @ clause2) cnf2)
+    [] cnf1
 
-let check_coloration (x:int) (y:int) (t:int) (possibles_colors: color list) (dim:int) : cnf =
+let check_coloration (x : int) (y : int) (t : int)
+    (possibles_colors : color list) (dim : int) : cnf =
   let rec check_coord_have_one_color colors =
-    match colors with 
-    | hd :: tl -> or_cnf_dev (check_have_not_color (x,y,t,hd) possibles_colors dim) (check_have_color ((x+1) mod dim,y,t,hd) possibles_colors dim) @
-    or_cnf_dev (check_have_not_color (x,y,t,hd) possibles_colors dim) (check_have_color (x,(y+1) mod dim,t,hd) possibles_colors dim) @
-    or_cnf_dev (check_have_not_color (x,y,t,hd) possibles_colors dim) (check_have_color ((x+dim-1) mod dim,y,t,hd) possibles_colors dim) @
-    or_cnf_dev (check_have_not_color (x,y,t,hd) possibles_colors dim) (check_have_color (x,(y+dim-1) mod dim,t,hd) possibles_colors dim) @
-    check_coord_have_one_color tl
+    match colors with
+    | hd :: tl ->
+        or_cnf_dev
+          (check_have_not_color (x, y, t, hd) possibles_colors dim)
+          (check_have_color ((x + 1) mod dim, y, t, hd) possibles_colors dim)
+        @ or_cnf_dev
+            (check_have_not_color (x, y, t, hd) possibles_colors dim)
+            (check_have_color (x, (y + 1) mod dim, t, hd) possibles_colors dim)
+        @ or_cnf_dev
+            (check_have_not_color (x, y, t, hd) possibles_colors dim)
+            (check_have_color
+               ((x + dim - 1) mod dim, y, t, hd)
+               possibles_colors dim)
+        @ or_cnf_dev
+            (check_have_not_color (x, y, t, hd) possibles_colors dim)
+            (check_have_color
+               (x, (y + dim - 1) mod dim, t, hd)
+               possibles_colors dim)
+        @ check_coord_have_one_color tl
     | [] -> []
-  in check_coord_have_one_color possibles_colors
+  in
+  check_coord_have_one_color possibles_colors
--- lib/graph.mli
+++ lib/graph.mli
@@ -1,3 +1,5 @@
+open SAT_Solver.Cnf
+
 type color = int
 type position = int * int
 
@@ -28,25 +30,13 @@ val breadth_first_search :
 val pp_vertex : Format.formatter -> vertex -> unit
 val pp_graph : Format.formatter -> graph -> unit
 
+val get_name_variable : int * int * int * int -> int -> string
 (** Function that converts a quadruplet into a string. *)
-val get_name_variable : (int*int*int*int) -> int -> string
 
+val get_value_variable : string -> int -> bool * int * int * int * int
 (** Function that converts a string into corresponding quadruplet. *)
-val get_value_variable : string -> int -> (bool*int*int*int*int)
-
-type symbol
-type literal
-type clause
-type cnf 
-type assignment 
-type model 
-
-val print_cnf : cnf -> unit
 
 val get_cnf_only_one_true : string list -> cnf
-
 val check_have_color : int * int * int * int -> color list -> int -> cnf
-
 val check_have_not_color : int * int * int * int -> color list -> int -> cnf
-
 val check_coloration : int -> int -> int -> color list -> int -> cnf
