79e2e8e4387a0a264775629f9d18b4fc3c299c7d
Gabin Dudillieu <gdudillieu@gmail.com>
2024-03-06 03:07:23 +0100

    feat(parser): Add function to parse a

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,5 @@
-let () = print_endline "Hello World!"
+open Prjt_mi_recolor.Graph
+open Prjt_mi_recolor.Parser
+
+let () =
+  pp_graph Format.std_formatter (init_toroidal_grid 0 5 5 (file_to_array "te"))
