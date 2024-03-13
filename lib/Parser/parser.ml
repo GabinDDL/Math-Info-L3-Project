3d6a240938be57dbeca45982ed045aecfbf749c5
Gabin Dudillieu <gdudillieu@gmail.com>
2024-03-13 22:51:08 +0100

    chore(parser): Add some comments and rename functions

--- lib/Parser/parser.ml
+++ lib/Parser/parser.ml
@@ -1,11 +1,11 @@
 exception Wrong_number_of_args
 
-let parse_arg_line_to_array l =
+let parse_argument_line_to_array l =
   let str_list = String.split_on_char ' ' l in
   let int_list = List.map (fun x -> int_of_string x) str_list in
   Array.of_list int_list
 
-let parse_dim ic =
+let parse_dimension ic =
   try
     let l = input_line ic in
     let dim_str_list = String.split_on_char ' ' l in
@@ -17,7 +17,7 @@ let parse_dim ic =
 let parse_file_to_array ic =
   let rec obtain_lines acc =
     try
-      let l = parse_arg_line_to_array (input_line ic) in
+      let l = parse_argument_line_to_array (input_line ic) in
       obtain_lines (l :: acc)
     with
     | End_of_file -> Ok (List.rev acc)
@@ -30,7 +30,7 @@ let parse_file_to_array ic =
 let parse_file_for_solver file =
   let ic = open_in file in
   let res =
-    match (parse_dim ic, parse_file_to_array ic) with
+    match (parse_dimension ic, parse_file_to_array ic) with
     | Error e, _ | _, Error e -> Error e
     | Ok dim, Ok a -> Ok (dim, a)
   in
--- lib/Parser/parser.mli
+++ lib/Parser/parser.mli
@@ -1,2 +1,3 @@
 val parse_file_for_solver :
   string -> ((int * int) * int array array, exn) result
+(** Takes a path to a file to parse it and return the information needed to create a graph *)
