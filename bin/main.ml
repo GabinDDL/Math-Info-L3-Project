494626c9193e79f38edfaa5ab2dbbe41dfd97c8d
Gabin Dudillieu <gdudillieu@gmail.com>
2024-03-13 16:08:43 +0100

    feat(main): Add functions in main to parse args

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,15 @@
 open Prjt_mi_recolor.Graph
 open Prjt_mi_recolor.Parser
 
+let run_solver_on_file file =
+    Format.printf "%s" file;
+  match parse_file_for_solver file with
+  | Error e -> raise e
+  | Ok ((l, w), a) ->
+      let grid = init_toroidal_grid 0 l w a in
+      pp_graph Format.std_formatter grid
+
 let () =
-  pp_graph Format.std_formatter (init_toroidal_grid 0 5 5 (file_to_array "te"))
+  let l = Array.length Sys.argv in
+  if l > 3 || l <= 1 then failwith "Not a good number of arg"
+  else run_solver_on_file Sys.argv.(1)
