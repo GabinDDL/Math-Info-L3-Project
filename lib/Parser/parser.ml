0ad950563ff88eb396179f0b627eccc9a9ab5d60
Gabin Dudillieu <gdudillieu@gmail.com>
2024-03-13 14:02:27 +0100

    feat(parser): Add parsing functions to create a graph

--- lib/Parser/parser.ml
+++ lib/Parser/parser.ml
@@ -1,16 +1,38 @@
-let line_to_array l =
+exception Wrong_number_of_args
+
+let parse_arg_line_to_array l =
   let str_list = String.split_on_char ' ' l in
   let int_list = List.map (fun x -> int_of_string x) str_list in
   Array.of_list int_list
 
-let file_to_array f =
-  let ic = open_in f in
+let parse_dim ic =
+  try
+    let l = input_line ic in
+    let dim_str_list = String.split_on_char ' ' l in
+    match dim_str_list with
+    | [ l; w ] -> Ok (int_of_string w, int_of_string l)
+    | _ -> raise Wrong_number_of_args
+  with e -> Error e
+
+let parse_file_to_array ic =
   let rec obtain_lines acc =
     try
-      let l = line_to_array (input_line ic) in
+      let l = parse_arg_line_to_array (input_line ic) in
       obtain_lines (l :: acc)
-    with End_of_file -> List.rev acc
+    with
+    | End_of_file -> Ok (List.rev acc)
+    | e -> Error e
   in
-  let lst_lines = obtain_lines [] in
+  match obtain_lines [] with
+  | Error e -> Error e
+  | Ok lst_lines -> Ok (Array.of_list lst_lines)
+
+let parse_file_for_solver file =
+  let ic = open_in file in
+  let res =
+    match (parse_dim ic, parse_file_to_array ic) with
+    | Error e, _ | _, Error e -> Error e
+    | Ok dim, Ok a -> Ok (dim, a)
+  in
   close_in ic;
-  Array.of_list lst_lines
+  res
--- lib/Parser/parser.mli
+++ lib/Parser/parser.mli
@@ -1,1 +1,1 @@
-val file_to_array : string -> int array array
+val parse_file_for_solver : string -> (((int * int) * (int array array)), exn) result
