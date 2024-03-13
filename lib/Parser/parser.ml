f9fe096cdcae4d3e6fd67601a76e4c57cbf5c532
Gabin Dudillieu <gdudillieu@gmail.com>
2024-03-13 23:02:32 +0100

    chore(parser): Add type on arguments

--- lib/Parser/parser.ml
+++ lib/Parser/parser.ml
@@ -1,11 +1,11 @@
 exception Wrong_number_of_args
 
-let parse_argument_line_to_array l =
+let parse_argument_line_to_array (l : string) : int array =
   let str_list = String.split_on_char ' ' l in
   let int_list = List.map (fun x -> int_of_string x) str_list in
   Array.of_list int_list
 
-let parse_dimension ic =
+let parse_dimension (ic : in_channel) : (int * int, exn) result =
   try
     let l = input_line ic in
     let dim_str_list = String.split_on_char ' ' l in
@@ -14,7 +14,7 @@ let parse_dimension ic =
     | _ -> raise Wrong_number_of_args
   with e -> Error e
 
-let parse_file_to_array ic =
+let parse_file_to_array (ic : in_channel) : (int array array, exn) result =
   let rec obtain_lines acc =
     try
       let l = parse_argument_line_to_array (input_line ic) in
@@ -27,7 +27,8 @@ let parse_file_to_array ic =
   | Error e -> Error e
   | Ok lst_lines -> Ok (Array.of_list lst_lines)
 
-let parse_file_for_solver file =
+let parse_file_for_solver (file : string) :
+    ((int * int) * int array array, exn) result =
   let ic = open_in file in
   let res =
     match (parse_dimension ic, parse_file_to_array ic) with
