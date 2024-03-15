53e18bd708ba14515844f9094dc92b2873675649
ARROUS Thomas <thomas_arrous@yahoo.fr>
2024-03-15 23:37:48 +0100

    add to mli

--- lib/cnf_graph.mli
+++ lib/cnf_graph.mli
@@ -10,17 +10,30 @@ val get_variable_value : string -> int -> bool * int * int * int * int
 val get_only_one_true_cnf : string list -> cnf
 (** Function to create a CNF that verifies that only one condition is true and the others are all false. *)
 
-val check_has_color : int * int * int * int -> color list -> int -> cnf
+val check_has_color : int * int * int * int -> color list -> int -> int -> cnf
 (** Function that generates the cnf to check if a node is colored with a specific color and not with the others. *)
 
-val check_has_not_color : int * int * int * int -> color list -> int -> cnf
+val check_has_not_color :
+  int * int * int * int -> color list -> int -> int -> cnf
 (** Function that generates the cnf to check if a node is not colored with a specific color and is colored with a single other. *)
 
-val check_coloration_of_one_node : int -> int -> int -> color list -> int -> cnf
+val check_coloration_of_one_node :
+  int -> int -> int -> color list -> int -> int -> cnf
 (** Function that generates the cnf to check if a node is correctly colored. *)
 
-val check_coloration_of_graph : int -> int -> int -> cnf
+val check_coloration_of_graph : int -> int -> int -> int list -> cnf
 (** Function that generates the cnf that says if a graph is well colored at an instant t*)
 
-val check_coloration_modification_of_graph : int -> int -> color list -> cnf
+val check_coloration_modification_of_graph :
+  int -> int -> int -> color list -> cnf
 (** Function that generates the cnf that says if a graph respects the rules of recoloring between t and t+1*)
+
+val check_coloration_start_and_final :
+  (int * int) * int array array -> int array array -> int -> color list -> cnf
+
+val get_cnf :
+  (int * int) * int array array ->
+  (int * int) * int array array ->
+  int ->
+  int ->
+  cnf
