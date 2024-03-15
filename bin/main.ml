6ca5d98b778a3927ef59571e46abf4fda8388ff9
Gabin Dudillieu <gdudillieu@gmail.com>
2024-03-15 22:25:39 +0100

    feat(main): Add possibility to give an output to the program

--- bin/main.ml
+++ bin/main.ml
@@ -1,16 +1,21 @@
 open Prjt_mi_recolor.Graph
 open Prjt_mi_recolor.Parser
 
-let run_solver_on_file file =
-  Format.printf "%s" file;
+let run_solver_on_file file file_output =
   match parse_file_for_solver file with
   | Error e -> raise e
-  | Ok ((l, w), a) ->
+  | Ok ((l, w), a) -> (
       let grid = init_toroidal_grid 0 w l a in
-      pp_graph Format.std_formatter grid
+      match file_output with
+      | None -> pp_graph Format.std_formatter grid
+      | Some output ->
+          let ic = open_out output in
+          pp_graph (Format.formatter_of_out_channel ic) grid;
+          close_out ic)
 (* TODO: To change with the result of SAT solver. *)
 
 let () =
   let l = Array.length Sys.argv in
-  if l > 3 || l <= 1 then failwith "Not a good number of arg"
-  else run_solver_on_file Sys.argv.(1)
+  if l = 2 then run_solver_on_file Sys.argv.(1) None
+  else if l = 3 then run_solver_on_file Sys.argv.(1) (Some Sys.argv.(2))
+  else failwith "Not a good number of arg"
