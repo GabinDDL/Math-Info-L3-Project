9b76544423bfe6a544452e5d135f9ae515493559
Gabin Dudillieu <gdudillieu@gmail.com>
2024-03-15 23:20:17 +0100

    feat(parser): Add the final grid to the parser

--- lib/Parser/parser.ml
+++ lib/Parser/parser.ml
@@ -1,4 +1,6 @@
 exception Wrong_number_of_args
+exception Wrong_width_of_array
+exception Wrong_separation
 
 let parse_argument_line_to_array (l : string) : int array =
   let str_list = String.split_on_char ' ' l in
@@ -14,26 +16,40 @@ let parse_graph_informations (ic : in_channel) : (int * int * int, exn) result =
     | _ -> raise Wrong_number_of_args
   with e -> Error e
 
-let parse_file_to_array (ic : in_channel) : (int array array, exn) result =
-  let rec obtain_lines acc =
-    try
-      let l = parse_argument_line_to_array (input_line ic) in
-      obtain_lines (l :: acc)
-    with
-    | End_of_file -> Ok (List.rev acc)
-    | e -> Error e
+let parse_file_to_array (ic : in_channel) ((w, l) : int * int) :
+    (int array array, exn) result =
+  let rec obtain_lines acc i =
+    if i = l then Ok (List.rev acc)
+    else
+      try
+        let l = parse_argument_line_to_array (input_line ic) in
+        if Array.length l = w then obtain_lines (l :: acc) (i + 1)
+        else raise Wrong_width_of_array
+      with e -> Error e
   in
-  match obtain_lines [] with
+  match obtain_lines [] 0 with
   | Error e -> Error e
   | Ok lst_lines -> Ok (Array.of_list lst_lines)
 
+let parse_graph_separator (ic : in_channel) : (unit, exn) result =
+  try
+    let sep = input_line ic in
+    if String.compare sep "#" <> 0 then raise Wrong_separation else Ok ()
+  with e -> Error e
+
 let parse_file_for_solver (file : string) :
-    ((int * int * int) * int array array, exn) result =
+    ((int * int * int) * int array array * int array array, exn) result =
   let ic = open_in file in
-  let res =
-    match (parse_graph_informations ic, parse_file_to_array ic) with
-    | Error e, _ | _, Error e -> Error e
-    | Ok info, Ok a -> Ok (info, a)
-  in
-  close_in ic;
-  res
+
+  match parse_graph_informations ic with
+  | Error e -> Error e
+  | Ok (t, w, l) -> (
+      match
+        ( parse_file_to_array ic (w, l),
+          parse_graph_separator ic,
+          parse_file_to_array ic (w, l) )
+      with
+      | Ok init_a, Ok (), Ok final_a ->
+          close_in ic;
+          Ok ((t, w, l), init_a, final_a)
+      | Error e, _, _ | _, Error e, _ | _, _, Error e -> Error e)
--- lib/Parser/parser.mli
+++ lib/Parser/parser.mli
@@ -1,3 +1,6 @@
+exception Wrong_number_of_args
+exception Wrong_width_of_array
+
 val parse_file_for_solver :
-  string -> ((int * int * int) * int array array, exn) result
+  string -> ((int * int * int) * int array array * int array array, exn) result
 (** Takes a path to a file to parse it and return the information needed to create a graph *)
